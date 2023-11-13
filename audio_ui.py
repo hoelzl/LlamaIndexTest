bc4d01d132ac33ac754be45f7bf2bbb6972238f6
Matthias Hölzl <tc@xantira.com>
2023-11-13 05:02:29 +0100

    Add speech recognition interface

--- audio_ui.py
+++ audio_ui.py
@@ -1,28 +1,133 @@
+# %%
+import logging
 import os
+import sys
 
 import gradio as gr
 import numpy as np
+from llama_index import (
+    BeautifulSoupWebReader,
+    ServiceContext,
+    SimpleDirectoryReader,
+    StorageContext,
+    VectorStoreIndex,
+    load_index_from_storage,
+)
+from llama_index.llms import Ollama
+from transformers import pipeline
+
+# %%
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+# %%
+# Possible values are "tiny", "base", "small", "medium", "large-v3"
+model_size = "base"
+# Forces the loading of the data into the vector database
+force_load = False
+# Use a simple local data set instead of scraping the web
+use_simple_data = False
+# Use the chat engine instead of the query engine
+use_chat_engine = True
 
 
-def reverse_audio(audio):
-    sr, data = audio
-    return sr, np.flipud(data)
+def cam(path):
+    return f"https://coding-academy.com/en/{path}"
 
 
-web_audio_files = ["https://samplelib.com/lib/preview/mp3/sample-3s.mp3"]
-local_audio_files = [
-    os.path.join(os.path.dirname(__file__), filename)
-    for filename in ["audio/what-is-cam.wav"]
+urls = [
+    cam(path)
+    for path in [
+        "",
+        "contact",
+        "about-us",
+        "programming-languages-machine-learning-open-training",
+        "python-basic-course-for-beginners-and-non-programmers",
+        "python-advanced-course-for-programmers-and-advanced",
+        "python-clean-code-and-clean-test-for-programmers-and-advanced",
+        "python-clean-software-architecture-and-design-patterns-"
+        "for-programmers-and-advanced",
+    ]
 ]
-local_audio_files = [file for file in local_audio_files if os.path.exists(file)]
 
-demo = gr.Interface(
-    fn=reverse_audio,
-    inputs=gr.Microphone(interactive=True, streaming=True),
-    outputs=gr.Audio(streaming=True),
-    examples=web_audio_files + local_audio_files,
-    cache_examples=True,
+llm = Ollama(model="llama2")
+service_context = ServiceContext.from_defaults(llm=llm, chunk_size=1000)
+
+# noinspection DuplicatedCode
+if use_simple_data:
+    if force_load or not os.path.exists("./storage"):
+        _documents = SimpleDirectoryReader("data").load_data()
+        vector_index = VectorStoreIndex.from_documents(_documents)
+        vector_index.storage_context.persist()
+    else:
+        _storage_context = StorageContext.from_defaults(persist_dir="./storage")
+        vector_index = load_index_from_storage(_storage_context)
+else:
+    if force_load or not os.path.exists("./storage"):
+        _documents = BeautifulSoupWebReader().load_data(urls)
+        vector_index = VectorStoreIndex.from_documents(_documents)
+        vector_index.storage_context.persist()
+    else:
+        _storage_context = StorageContext.from_defaults(persist_dir="./storage")
+        vector_index = load_index_from_storage(_storage_context)
+
+
+# %%
+transcriber = pipeline(
+    "automatic-speech-recognition", model=f"openai/whisper-{model_size}"
 )
 
+
+# %%
+def transcribe(new_chunk):
+    logging.info(f"Transcribing {new_chunk}")
+    if new_chunk is None:
+        return {_text: _text}
+    sr, y = new_chunk
+    y = y.astype(np.float32)
+    y /= np.max(np.abs(y), initial=1.0)
+    return {_text: transcriber({"sampling_rate": sr, "raw": y})["text"]}
+
+
+# noinspection DuplicatedCode
+if use_chat_engine:
+    engine = vector_index.as_chat_engine(cache=None, similarity_top_k=6)
+else:
+    engine = vector_index.as_query_engine(cahce=None, similarity_top_k=6)
+
+
+def response(message):
+    try:
+        logging.info(f"Response for {message}")
+        result = engine.chat(message) if use_chat_engine else engine.query(message)
+        logging.info(f"Answer is {result}")
+        return str(result)
+    except:
+        return "Sorry, an error occurred."
+
+
+def transcribe_and_reply(recording):
+    if recording is None:
+        return "No recording", ""
+    text = transcribe(recording)
+    answer = response(text)
+    return text, answer
+
+
+def clear_recording():
+    return {_recording: None}
+
+
+app = gr.Blocks()
+
+with app:
+    _recording = gr.Microphone()
+    _text = gr.Textbox()
+    _label = gr.Label()
+
+    _recording.change(transcribe, inputs=_recording, outputs={_text})
+    _text.change(response, inputs=_text, outputs=[_label])
+    _label.change(clear_recording, outputs={_recording})
+
+
 if __name__ == "__main__":
-    demo.launch()
+    app.launch()
